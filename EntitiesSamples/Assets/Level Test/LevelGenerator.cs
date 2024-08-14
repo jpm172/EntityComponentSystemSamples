216cbf8299cb63ac2a36c0db3c72400937219a3c
jpm172 <moynahanjaredp@gmail.com>
2024-08-14 16:53:37 -0400

    basic level seeding

--- EntitiesSamples/Assets/Level Test/LevelGenerator.cs
+++ EntitiesSamples/Assets/Level Test/LevelGenerator.cs
@@ -31,6 +31,8 @@ public partial class LevelGenerator : MonoBehaviour
     private List<LevelFloor> _floors;
     private List<LevelWall> _walls;
     private LevelRoom[] _rooms;
+    
+    
     private NativeArray<int> _levelLayout;
     
     
@@ -41,6 +43,7 @@ public partial class LevelGenerator : MonoBehaviour
         if ( !_levelLayout.IsCreated )
             return;
         
+        Vector3 size = Vector3.one / GameSettings.PixelsPerUnit;
         for ( int x = 0; x < dimensions.x; x++ )
         {
             for ( int y = 0; y < dimensions.y; y++ )
@@ -49,15 +52,31 @@ public partial class LevelGenerator : MonoBehaviour
                 if ( _levelLayout[index] > 0 )
                 {
                     LevelRoom room = _rooms[_levelLayout[index] - 1];
-                    Vector3 pos = new Vector3(x,y);
+                    Vector3 pos = new Vector3(x,y)/GameSettings.PixelsPerUnit;
+                    
                     Gizmos.color = room.DebugColor;
-                    Gizmos.DrawCube( pos, Vector3.one );
+                    Gizmos.DrawCube( pos, size );
+                    //Gizmos.DrawWireCube( pos,size );
+                }
+                else if ( IsBorder( x, y ) )
+                {
+                    Gizmos.color = Color.black;
+                    Vector3 pos = new Vector3(x,y)/GameSettings.PixelsPerUnit;
+                    Gizmos.DrawCube( pos, size );
                 }
             }
         }
     }
 #endif
 
+    private bool IsBorder( int x, int y )
+    {
+        if ( x == 0 || x == dimensions.x - 1 )
+            return true;
+        if ( y == 0 || y == dimensions.y - 1 )
+            return true;
+        return false;
+    }
 
     public void GenerateLevel()
     {
@@ -73,7 +92,9 @@ public partial class LevelGenerator : MonoBehaviour
         int minSize = 20;
         int count = layoutDimensions.x * layoutDimensions.y;
 
-        _levelLayout = new NativeArray<int>(dimensions.x*dimensions.y, Allocator.Persistent);
+        if ( _levelLayout.IsCreated )
+            _levelLayout.Dispose();
+        
         _floors = new List<LevelFloor>();
         _walls = new List<LevelWall>();
         _rooms = new LevelRoom[count];
@@ -81,18 +102,52 @@ public partial class LevelGenerator : MonoBehaviour
         
         //create the rooms and seed them into the level layout
 
+        int buffer = 50;
+        int xOffset = buffer;
+        
         for ( int x = 0; x < layoutDimensions.x; x++ )
         {
+            int yOffset = buffer;
+
             for ( int y = 0; y < layoutDimensions.y; y++ )
             {
-                LevelRoom room = new LevelRoom(x+y+1, Random.Range( 2,6 ));
-                _rooms[x + y] = room;
-                _levelLayout[x + y] = room.Id;
+                int index = x + y * layoutDimensions.x;
+                Vector2Int roomOrigin = new Vector2Int(xOffset, yOffset);
+                Vector2Int roomSize = new Vector2Int( Random.Range( minSize, buffer ), Random.Range( minSize, buffer ) );
+                LevelRoom room = new LevelRoom(index+1, roomOrigin, roomSize, Random.Range( 2,6 ));
+                
+                _rooms[index] = room;
+                
+                yOffset += buffer*2;
             }
+            
+            xOffset += buffer*2;
         }
         
-
+        //create the level array aand seed it with the rooms  
+        dimensions = new Vector2Int(buffer*layoutDimensions.x*2 + buffer , buffer*layoutDimensions.y*2 + buffer );
+        _levelLayout = new NativeArray<int>(dimensions.x*dimensions.y, Allocator.Persistent);
+        for(int i = 0; i < _rooms.Length; i++)
+        {
+            LevelRoom room = _rooms[i];
+            DrawBox( room.Origin.x, room.Origin.y, room.Size.x, room.Size.y, ref room );
+        }
         
+    }
+
+    private void DrawBox( int xOrigin, int yOrigin, int width, int height, ref LevelRoom room )
+    {
+        int start = xOrigin + yOrigin * dimensions.x;
+
+        for ( int x = 0; x < width; x++ )
+        {
+            for ( int y = 0; y < height; y++ )
+            {
+                int index = x + y * dimensions.x;
+                _levelLayout[start + index] = room.Id;
+            }
+        }
+
     }
     
     private void MakeWalls()
@@ -163,8 +218,11 @@ public partial class LevelGenerator : MonoBehaviour
 
     private void OnDestroy()
     {
-        if(_levelLayout.IsCreated)
+        if ( _levelLayout.IsCreated )
+        {
             _levelLayout.Dispose();
+        }
+            
     }
 }
 
@@ -174,15 +232,21 @@ public struct LevelRoom
     private int _wallThickness;
     private int _id;
     private Color _debugColor;
+    private Vector2Int _size;
+    private Vector2Int _origin;
 
+    public Vector2Int Size => _size;
+    public Vector2Int Origin => _origin;
     public int WallThickness => _wallThickness;
     public int Id => _id;
     public Color DebugColor => _debugColor;
     
 
-    public LevelRoom( int id, int wallThickness )
+    public LevelRoom( int id, Vector2Int origin, Vector2Int size, int wallThickness )
     {
         _id = id;
+        _origin = origin;
+        _size = size;
         _wallThickness = wallThickness;
         _debugColor = new Color(Random.Range( 0,1f ),Random.Range( 0,1f ),Random.Range( 0,1f ), 1);
     }
