7017d96adb8a58f1a9470c250cd6ffc8f7a13375
jpm172 <moynahanjaredp@gmail.com>
2024-10-14 10:02:11 -0400

    more camera stuff

--- EntitiesSamples/Assets/Level Test/Character/CameraFollow.cs
+++ EntitiesSamples/Assets/Level Test/Character/CameraFollow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -11,17 +12,21 @@ public partial class CameraFollowSystem : SystemBase
 
     private EntityQuery playerQuery;
     private static float3 offset = new float3(0,0, -10);
-
+    private Transform _playerTracker;
+    
+    
     protected override void OnCreate()
     {
         playerQuery = SystemAPI.QueryBuilder().WithAll<PlayerInputs, LocalTransform>().Build();
+        _playerTracker = GameObject.FindGameObjectWithTag( "Player" ).transform;
         RequireForUpdate( playerQuery );
     }
 
     protected override void OnUpdate()
     {
         LocalTransform t = playerQuery.GetSingleton<LocalTransform>();
-        Camera.main.transform.position = t.Position + offset;
+        _playerTracker.position = t.Position;
+        //Camera.main.transform.position = t.Position + offset;
 
     }
 }
