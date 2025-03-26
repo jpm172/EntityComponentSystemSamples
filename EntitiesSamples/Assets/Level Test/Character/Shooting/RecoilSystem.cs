ed2f991de132f1dd48d222fc116c4c295fd74ca7
jpm172 <moynahanjaredp@gmail.com>
2025-03-26 16:48:03 -0400

    figured out math for relative crosshair recoil

--- EntitiesSamples/Assets/Level Test/Character/Shooting/RecoilSystem.cs
+++ EntitiesSamples/Assets/Level Test/Character/Shooting/RecoilSystem.cs
@@ -33,22 +33,35 @@ public partial struct RecoilJob : IJobEntity
 
     private void Execute( ref LocalTransform transform, ref PlayerInputs input )
     {
-        input.RecoilOffset = new float3(1,0,0);
+        input.RecoilOffset = new float3(-4,0,0);
+        float3 relativeRecoil = input.RecoilOffset;
         float3 forward = math.normalizesafe( transform.Position-input.AimPosition );
-
+        
+        
         
         float3 right = math.rotate( quaternion.RotateZ( math.radians( 90 ) ), forward );
-        float3 left = math.rotate( quaternion.RotateZ( math.radians( -90 ) ), forward );
-        
-        
+
+
+        float relativeAngle = math.radians( 90 * math.sign( input.RecoilOffset.x ));
+        relativeRecoil = math.rotate( quaternion.RotateZ( relativeAngle ), forward  )* math.length( input.RecoilOffset );
+
         Debug.DrawLine( input.AimPosition, input.AimPosition + forward, Color.green, 0.1f );
         Debug.DrawLine( input.AimPosition, input.AimPosition + right, Color.red, 0.1f );
-        Debug.DrawLine( input.AimPosition, input.AimPosition + left, Color.blue, 0.1f );
+        Debug.DrawLine( input.AimPosition, input.AimPosition - right, Color.blue, 0.1f );
 
-        //Debug.DrawLine( input.AimPosition, right, Color.red, 0.1f );
 
+        DrawRecoil( input, relativeRecoil );
         
         
+        
+    }
+
+    private void DrawRecoil(PlayerInputs input, float3 pos)
+    {
+        float3 center = input.AimPosition + pos;
+        float size = 0.25f;
+        Debug.DrawLine( center + new float3(-size, -size, 0) , center+ new float3(size, size, 0) , Color.yellow, 0.1f );
+        Debug.DrawLine( center + new float3(-size, size, 0) , center+ new float3(size, -size, 0) , Color.yellow, 0.1f );
         
     }
 
